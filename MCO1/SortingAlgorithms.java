9eecf5b9ee7325c32954b3452c71abb27391167d
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2023-06-12 17:31:38 +0800

    Implement radix sort

--- MCO1/SortingAlgorithms.java
+++ MCO1/SortingAlgorithms.java
@@ -103,10 +103,86 @@ public class SortingAlgorithms {
      * records
      */
 
-    public void radixSort(Record[] arr, int p, int r) {
+    /**
+     * T(n) = k(11n + 35) + 2, where k = max no. of digits in <code>arr</code>.
+     */
+    public void radixSort(Record[] arr, int n) {
+        int arrMax = max(arr, n);                                                   // 1
 
-        // TODO: Implement this sorting algorithm here.
-
+        for (int k = 1; arrMax / k > 0; k *= 10) {                                  // k + 1, where k = max no. of digits in arr
+            countingSort(arr, n, k);                                                // (k)(11n + 34)
+        } 
     }
 
+    /**
+     * Implemented for <code>radixSort()</code>.
+     * T(n) = 11n + 34
+     */
+    private void countingSort(Record[] arr, int n, int k) {       
+        int i;                                                                      // 1
+        int[] counts = new int[10];                                                 // 1
+        Record[] output = new Record[n];                                            // 1
+
+        // Count the no. of occurrences of the kth digit in each element of arr.
+        for (i = 0; i < n; i++) {                                                   // 1 + (n - 0 + 1) + n = 2n + 2
+            counts[kthDigit(arr[i].getIdNumber(), k)]++;                            // n
+        }
+
+        // Get the position of the last occurrence of a digit in the output 
+        // array by computing the cummulative sum of the values in the count 
+        // array.
+        for (i = 1; i < counts.length; i++) {                                       // 1 + (10 - 1 + 1) + 9 = 20
+            counts[i] += counts[i - 1];                                             // 9
+        }
+
+        // Build the output array, now knowing the where the last occurrence of
+        // each digit should be in the output array. 
+        // 
+        // As only the ending position of each digit in the output array is 
+        // known, each digit in the output array will be written backwards, 
+        // starting from the ending position until no more occurrences of a 
+        // digit are left.
+        //
+        // Hence, to keep this algorithm stable, the input array should also be
+        // traversed backwards so that the last occurrence in the input will end
+        // up at its last position in the output, and so on.
+        for (i = n - 1; i >= 0; i--) {                                              // 1 + (n - 1 - 0 + 2) + (n - 1) = 2n + 1
+            int d = kthDigit(arr[i].getIdNumber(), k);                              // n - 1
+
+            // Subtract 1 to get the index since each count starts from 1.
+            output[counts[d] - 1] = arr[i];                                         // n - 1
+            counts[d]--;                                                            // n - 1
+        }
+
+        // Copy the sorted output into the given array. The given array cannot
+        // be directly worked on as the necessary values to work on will be 
+        // overwritten in the process.
+        for (i = 0; i < n; i++) {                                                   // 1 + (n - 0 + 1) + n = 2n + 2
+            arr[i] = output[i];                                                     // n
+        }
+    }
+
+    /**
+     * Implemented for <code>radixSort()</code>.
+     * T(n) = 1
+     */
+    private int kthDigit(int num, int k) {
+        return (num / k) % 10;                                                      // 1
+    }
+
+    /**
+     * Implemented for <code>radixSort()</code>.
+     * T(n) = 4n + 4
+     */
+    private int max(Record[] arr, int n) {
+        int maxVal = arr[0].getIdNumber();                                          // 1
+
+        for (int i = 0; i < n; i++) {                                               // 1 + (n - 0 + 1) + n = 2n + 2
+            if (arr[i].getIdNumber() > maxVal) {                                    // n
+                maxVal = arr[i].getIdNumber();                                      // n
+            }
+        }
+
+        return maxVal;                                                              // 1
+    }
 }
